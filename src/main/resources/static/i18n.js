a9b3ed9f60d843edb57283079b080826665e517c
Noya07 <vilius.ramoska@gmail.com>
2020-07-10 12:22:21 +0200

    A touch of css to beautify the flags

--- src/main/resources/static/i18n.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#language").change(function () {
-        var selectedOption = $('#language').val();
-        if (selectedOption != ''){
-            window.location.replace('rsvp?lang=' + selectedOption);
-        }
-    });
-});
